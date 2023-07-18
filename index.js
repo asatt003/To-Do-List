b78c68dd9c11cbe8c5c250bc16ca694e2912eb24
Allyn <allyn_sattler@yahoo.com>
2023-07-18 09:44:55 -0700

    Correctly outputted to-do list items under the user input box.

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 let nameField = document.querySelector('#name');
 let userInput = "";
 var userInputArray = [];
-function storeInput(input) {
-    //console.log(`input : ${input}`);
-    return userInputArray.push(input);
+function itemConstructor(inputArray) {
+  let newContainer = document.querySelector('#todoListContainer');
+  let newElement = document.createElement('div');
+  newElement.innerHTML = inputArray[(inputArray.length - 1)];
+  newContainer.appendChild(newElement);
+  return;
 };
 nameField.addEventListener(
-    "input", 
-    (eventObject) => {userInput = eventObject.target.value}
+  "input", 
+  (eventObject) => {userInput = eventObject.target.value}
 );
 let btn = document.querySelector('#btn');
 btn.addEventListener("click", () => {
-    storeInput(userInput);
-    console.log(userInputArray);
-});
+  userInputArray.push(userInput);
+  itemConstructor(userInputArray);
   //console.log(userInputArray);
+});
