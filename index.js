b02e0b1b97ecaf4e1745ea36d55284b92acc7e5c
Allyn Sattler <allyn_sattler@yahoo.com>
2023-07-18 18:18:50 -0700

    Update index.js
    Removed comments.

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-/*let nameField = document.querySelector('#name');
-let userInput = "";
-var userInputArray = [];
-function itemConstructor(inputArray) {
-  let newContainer = document.querySelector('#todoListContainer');
-  let newElement = document.createElement('div');
-  newElement.innerHTML = inputArray[(inputArray.length - 1)];
-  newContainer.appendChild(newElement);
-  return;
-};
-nameField.addEventListener(
-  "input", 
-  (eventObject) => {userInput = eventObject.target.value}
-);
-let btn = document.querySelector('#btn');
-btn.addEventListener("click", () => {
-  userInputArray.push(userInput);
-  itemConstructor(userInputArray);
-  //console.log(userInputArray);
-});*/
-
 let nameField = document.querySelector('#name');
 let userInput = { text: "", identifier: 0 };
 let count = 0;
