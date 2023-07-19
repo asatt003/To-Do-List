a62a0c1b6a8cb2603b37f480af95e46f702fa220
Allyn <allyn_sattler@yahoo.com>
2023-07-18 18:00:42 -0700

    Final build.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let nameField = document.querySelector('#name');
+/*let nameField = document.querySelector('#name');
 let userInput = "";
 var userInputArray = [];
 function itemConstructor(inputArray) {
@@ -17,4 +17,31 @@ btn.addEventListener("click", () => {
   userInputArray.push(userInput);
   itemConstructor(userInputArray);
   //console.log(userInputArray);
+});*/
+
+let nameField = document.querySelector('#name');
+let userInput = { text: "", identifier: 0 };
+let count = 0;
+var userInputArray = [];
+function itemConstructor(inputArray, count) {
+  let newContainer = document.querySelector('#todoListContainer');
+  let newElement = document.createElement('div');
+  newElement.setAttribute("id", count);
+  newElement.setAttribute("class", "Item");
+  newElement.innerHTML = (inputArray[(inputArray.length - 1)]).text;
+  newElement.addEventListener("click", () => newContainer.removeChild(newElement));
+  newContainer.appendChild(newElement);
+  return;
+};
+nameField.addEventListener(
+  "input", 
+  (eventObject) => {userInput.text = eventObject.target.value}
+);
+let btn = document.querySelector('#btn');
+btn.addEventListener("click", () => {
+  count++;
+  userInput.identifier = count;
+  userInputArray.push(userInput);
+  itemConstructor(userInputArray, count);
+  document.getElementById('name').value = "";
 });
